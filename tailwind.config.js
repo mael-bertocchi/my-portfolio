35b02fb82ecd78a3b9f43fa8701ec056de2f1745
Maël Bertocchi <ma3l.bth@gmail.com>
2025-11-07 14:15:33 +0100

    feat: migrate to static site with Vite and vanilla TypeScript

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,js,html}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        accent: '#821b29',
+      },
+    },
+  },
+  plugins: [],
+}
