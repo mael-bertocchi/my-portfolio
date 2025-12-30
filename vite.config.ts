55cf669bce5dc4e9775d26f8fa20464c1ce65c1b
Maël Bertocchi <ma3l.bth@gmail.com>
2025-12-30 12:52:28 +0100

    feat: add auto-generated sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
 import { defineConfig } from 'vite';
+import sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
+  plugins: [
+    sitemap({
+      hostname: 'https://mael-bertocchi.fr',
+      changefreq: {
+        '/': 'monthly',
+        '/legal': 'yearly'
+      },
+      priority: {
+        '/': 1.0,
+        '/legal': 0.5
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@assets': '/src/assets',
